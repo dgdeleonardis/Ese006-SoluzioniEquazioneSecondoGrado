4fc567231e19d243438ba0eaca7b6f4b89279867
Diego De Leonardis <diegowazzaap@hotmail.it>
2015-02-07 09:56:37 +0100

    Primo Commit

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "funzioni.h"
 
 int main(int argc, char** argv) {
-
+    float a, b, c, soluzioneuno, soluzionedue;
+    int onaco;
+    printf("Inserisci il valore di a");
+    scanf("%f", &a);
+    printf("Inserisci il valore di b");
+    scanf("%f", &b);
+    printf("Inserisci il valore di c");
+    scanf("%f", &c);
+    onaco = esistonoSoluzioniReali(a,b,c);
+    if (onaco) {
+        esistonoSoluzioniReali(a , b, c);
+    soluzioneuno = calcolaSoluzioneUno(a, b, c);
+    soluzionedue = calcolaSoluzioneDue(a, b, c);
+    printf("Le soluzioni sono %5.2f e %5.2f", soluzioneuno, soluzionedue);
+    }
+    else {
+        printf("niente");
+    }
     return (EXIT_SUCCESS);
 }
 
